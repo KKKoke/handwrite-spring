e92c69b2f04d845057c6263b66a035bb9948dafd
kkkoke <1120746320@qq.com>
2023-05-18 23:36:12 +0800

    add: 完成 step-06 实例化策略与 Bean 定义信息解析器

---
+++ step-06/src/main/java/com/kkkoke/springframework/beans/factory/support/BeanDefinitionReader.java
@@ -0,0 +1,25 @@
+package com.kkkoke.springframework.beans.factory.support;
+
+import com.kkkoke.springframework.beans.BeansException;
+import com.kkkoke.springframework.core.io.Resource;
+import com.kkkoke.springframework.core.io.ResourceLoader;
+
+/**
+ * @author KeyCheung
+ * @date 2023/05/17
+ * @desc
+ */
+public interface BeanDefinitionReader {
+
+    BeanDefinitionRegistry getRegistry();
+
+    ResourceLoader getResourceLoader();
+
+    void loadBeanDefinitions(Resource resource) throws BeansException;
+
+    void loadBeanDefinitions(Resource... resources) throws BeansException;
+
+    void loadBeanDefinitions(String location) throws BeansException;
+
+    void loadBeanDefinitions(String... locations) throws BeansException;
+}
